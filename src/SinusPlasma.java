4c4c2c82346f718ac5f7b701e11da17c9884ca17
Erhan Justice <bountyx@gmail.com>
2016-08-17 02:57:20 -0700

    Added sound support and tracking for SinusPlasma

--- src/SinusPlasma.java
+++ src/SinusPlasma.java
@@ -1,5 +1,7 @@
 import processing.core.*;
 import java.util.*;
+import ddf.minim.*;
+import ddf.minim.analysis.*;
 
 public class SinusPlasma extends PApplet {
 
@@ -7,14 +9,18 @@ public class SinusPlasma extends PApplet {
     int height;
     Tunnel tunnel;
     
+    AudioInput audio;
+    Minim minim;
+    BeatDetect beat;
+
     int GRADIENTLEN = 1500;
     // use this factor to make things faster, esp. for high resolutions
     int SPEEDUP = 3;
-    
+
     // swing/wave function parameters
     int SWINGLEN = GRADIENTLEN*3;
     int SWINGMAX = GRADIENTLEN / 2 - 1;
-    
+
     // gradient & swing curve arrays
     private int[] colorGrad;
     private int[] swingCurve;
@@ -24,6 +30,7 @@ public class SinusPlasma extends PApplet {
         width = w;
         height = h;
         tunnel = t;
+        audio = tunnel.in;
     }
 
     public void settings() {
@@ -31,22 +38,48 @@ public class SinusPlasma extends PApplet {
     }
 
 
-  
+
   // create standard gradient & swing curve
   public void setup() {
-  
+    minim = new Minim(this);
+    beat = new BeatDetect();
   	makeGradient(GRADIENTLEN);
   	makeSwingCurve(SWINGLEN, SWINGMAX);
   }
-  
+
+  float trackX = 0;
+  float trackY = 0;
+  float trackZ = 0;
+
+  public void track() {
+   if(Main.kinect != null) {
+       Main.kinect.update();
+       trackX = (float)width * Main.kinect.RightHandDepthRatio;
+       trackY = (float)height * Main.kinect.RightHandRaisedRatio;
+       trackZ = 0;
+   } else {
+       trackX = mouseX;
+       trackY = mouseY;
+   }
+  }
+
   // draw one frame of the sinus plasma
   public void draw() {
     synchronized (Tunnel.class) {
+      track();
+      
+      beat.detect(audio.mix);
+            if(beat.isOnset() && trackX > trackY) {
+               makeGradient(GRADIENTLEN);
+            } else if(beat.isOnset()) {
+              makeSwingCurve(SWINGLEN, SWINGMAX);
+            }
+            
     	loadPixels();
     	int i = 0;
     	int t = frameCount*SPEEDUP;
     	int swingT = swing(t); // swingT/-Y/-YT variables are used for a little tuning ...
-    
+
     	for (int y = 0; y < height; y++) {
     		int swingY  = swing(y);
     		int swingYT = swing(y + t);
@@ -62,7 +95,7 @@ public class SinusPlasma extends PApplet {
     	updatePixels();
     }
   }
-  
+
   // create a new random gradient when mouse is pressed
   public void mousePressed() {
   	if (mouseButton == LEFT)
@@ -70,7 +103,7 @@ public class SinusPlasma extends PApplet {
   	else if (mouseButton == RIGHT)
   		makeSwingCurve(SWINGLEN, SWINGMAX);
   }
-    
+
   // fill the given array with a nice swingin' curve
   // three cos waves are layered together for that
   // the wave "wraps" smoothly around, uh, if you know what i mean ;-)
@@ -79,7 +112,7 @@ public class SinusPlasma extends PApplet {
   	int factor1=2;
   	int factor2=3;
   	int factor3=6;
-  
+
   	if (swingCurve == null) {
   		swingCurve = new int[SWINGLEN];
   	} else {
@@ -87,9 +120,9 @@ public class SinusPlasma extends PApplet {
   		factor2=(int) random(1, 7);
   		factor3=(int) random(1, 7);
   	}
-  
+
   	int halfmax = maxval/factor1;
-  
+
   	for( int i=0; i<arrlen; i++ ) {
   		float ni = i*TWO_PI/arrlen; // ni goes [0..TWO_PI] -> one complete cos wave
   		swingCurve[i]=(int)(
@@ -99,7 +132,7 @@ public class SinusPlasma extends PApplet {
   			halfmax + halfmax );
   	}
   }
-  
+
   // create a smooth, colorful gradient by cosinus curves in the RGB channels
   private void makeGradient(int arrlen) {
   	// default values will be used upon first call
@@ -109,7 +142,7 @@ public class SinusPlasma extends PApplet {
   	int rd = 0;
   	int gd = arrlen / gf;
   	int bd = arrlen / bf / 2;
-  
+
   	if (colorGrad == null) {
   		// first call
   		colorGrad = new int[GRADIENTLEN];
@@ -121,9 +154,9 @@ public class SinusPlasma extends PApplet {
   		rd = (int) random(0, arrlen);
   		gd = (int) random(0, arrlen);
   		bd = (int) random(0, arrlen);
-  		System.out.println("Gradient factors("+rf+","+gf+","+bf+"), displacement("+rd+","+gd+","+bd+")");
+  		//System.out.println("Gradient factors("+rf+","+gf+","+bf+"), displacement("+rd+","+gd+","+bd+")");
   	}
-  
+
   	// fill gradient array
   	for (int i = 0; i < arrlen; i++) {
   		int r = cos256(arrlen / rf, i + rd);
@@ -132,17 +165,17 @@ public class SinusPlasma extends PApplet {
   		colorGrad[i] = color(r, g, b);
   	}
   }
-  
+
   // helper: get cosinus sample normalized to 0..255
   private int cos256(final int amplitude, final int x) {
   	return (int) (cos(x * TWO_PI / amplitude) * 127 + 60);
   }
-  
+
   // helper: get a swing curve sample
   private int swing(final int i) {
   	return swingCurve[i % SWINGLEN];
   }
-  
+
   // helper: get a gradient sample
   private int gradient(final int i) {
   	return colorGrad[i % GRADIENTLEN];
