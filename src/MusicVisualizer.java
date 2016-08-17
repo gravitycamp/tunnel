2ab6d1c4d0eaa36607e14f647b860cad1ac78e39
Erhan Justice <bountyx@gmail.com>
2016-08-16 23:37:07 -0700

    Updated MusicVisualizer to support Kinect. Color enhancements too

--- src/MusicVisualizer.java
+++ src/MusicVisualizer.java
@@ -7,19 +7,30 @@ public class MusicVisualizer extends PApplet {
 
     int   width;
     int   height;
+    
+    float trackX = 0;
+    float trackY = 0;
+    float trackZ = 0;
+
     Tunnel tunnel;
+
     Minim minim;
     BeatDetect beat;
+
     int  r = 200;
     float rad = 70;
-    AudioInput in;
+    int periode = 5000;
+    int displace = 250;
+    int displace2 = 500;
+
+    AudioInput player;
 
     public MusicVisualizer(Tunnel t, int w, int h) {
         width  = w;
         height = h;
         tunnel = t;
-        in =tunnel.in;
-        
+        player = tunnel.in;
+
     }
 
   public void settings() {
@@ -27,51 +38,73 @@ public class MusicVisualizer extends PApplet {
   }
   public void setup()
   {
-
     minim = new Minim(this);
-  
     beat = new BeatDetect();
     background(0);
   }
   
+  public void track() {
+   if(Main.kinect != null) {
+       Main.kinect.update();
+       trackX = (float)width * Main.kinect.RightHandDepthRatio;
+       trackY = (float)height * Main.kinect.RightHandRaisedRatio;
+       trackZ = 0;
+   } else {
+       trackX = mouseX;
+       trackY = mouseY;
+   }
+  }
+
   public void draw()
-  { 
-    background(0);
-    float t = map(mouseX, 0, width, 0, 1);
-    beat.detect(in.mix);
-    fill(1711896, 20);
-    noStroke();
-    rect(0, 0, width, height);
-    translate(width/2, height/2);
-    noFill();
-    fill(-1, 10);
-    if (beat.isOnset()) rad = (float)(rad*0.9);
-    else rad = 70;
-    ellipse(0, 0, 2*rad, 2*rad);
-    stroke(-1, 50);
-    int bsize = in.bufferSize();
-    for (int i = 0; i < bsize - 1; i+=5)
-    {
-      float x = (r)*cos(i*2*PI/bsize);
-      float y = (r)*sin(i*2*PI/bsize);
-      float x2 = (r + in.left.get(i)*100)*cos(i*2*PI/bsize);
-      float y2 = (r + in.left.get(i)*100)*sin(i*2*PI/bsize);
-      line(x, y, x2, y2);
+  {
+    synchronized (Tunnel.class) {
+        background(0);
+        track();
+     
+       if (trackX/2 > 100)
+         r = (int)trackX/2;
+
+       beat.detect(player.mix);
+       //fill(#1A1F18, 20);
+       noStroke();
+       rect(0, 0, width, height);
+       translate(width/2, height/2);
+       noFill();
+
+       float time = millis();
+       int red = (int)(128+127*cos(2*PI/periode*time));
+       int green = (int)(128+127*cos(2*PI/periode*(displace-time)));
+       int blue = (int)(128+127*cos(2*PI/periode*(displace2-time)));
+       fill(red, green, blue);
+       if (beat.isOnset()) rad = (float)(rad*0.9);
+       else rad = 70 + trackY/8;
+       ellipse(0, 0, 2*rad, 2*rad);
+       stroke(random(255), random(255), random(255));
+       int bsize = player.bufferSize();
+       for (int i = 0; i < bsize - 1; i+=5)
+       {
+         float x = (r)*cos(i*2*PI/bsize);
+         float y = (r)*sin(i*2*PI/bsize);
+         float x2 = (r + player.left.get(i)*100)*cos(i*2*PI/bsize);
+         float y2 = (r + player.left.get(i)*100)*sin(i*2*PI/bsize);
+         line(x, y, x2, y2);
+       }
+       beginShape();
+       noFill();
+         stroke(random(255), random(255), random(255));
+       for (int i = 0; i < bsize; i+=30)
+       {
+         float x2 = (r + player.left.get(i)*100)*cos(i*2*PI/bsize);
+         float y2 = (r + player.left.get(i)*100)*sin(i*2*PI/bsize);
+         vertex(x2, y2);
+         pushStyle();
+         stroke(random(255), random(255), random(255));
+         strokeWeight(2);
+         point(x2, y2);
+         popStyle();
+       }
+
+       endShape();
     }
-    beginShape();
-    noFill();
-    stroke(-1, 50);
-    for (int i = 0; i < bsize; i+=30)
-    {
-      float x2 = (r + in.left.get(i)*100)*cos(i*2*PI/bsize);
-      float y2 = (r + in.left.get(i)*100)*sin(i*2*PI/bsize);
-      vertex(x2, y2);
-      pushStyle();
-      stroke(-1);
-      strokeWeight(2);
-      point(x2, y2);
-      popStyle();
-    }
-    endShape();
   }
 }
